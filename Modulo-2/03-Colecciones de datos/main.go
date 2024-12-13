55f0a63a200223a45cd9074b6e3daff3b327ed8a
gianlucafarias <palmiergianluca@gmail.com>
2024-12-13 11:33:07 -0300

    colecciones y add temario

--- Modulo-2/03-Colecciones de datos/main.go
+++ Modulo-2/03-Colecciones de datos/main.go
@@ -20,4 +20,38 @@ func main() {
 
 	fmt.Println(frutas)
 	fmt.Println(verduras)
+
+	// Maps
+	var edades map[string]int
+	edades = make(map[string]int)
+	edades["Gian"] = 27
+	edades["Guille"] = 28
+	edades["Ale"] = 31
+
+	edades2 := map[string]int{
+		"Gian":   27,
+		"Guille": 28,
+		"Ale":    31,
+	}
+
+	fmt.Println(edades["Gian"])
+	fmt.Println(edades2["Ale"])
+
+	// Range
+	numerosRange := []int{10, 20, 30, 40, 50, 60, 70, 80, 90}
+
+	for i, num := range numerosRange {
+		fmt.Println(i, num)
+	}
+
+	// Range map
+	capitales := map[string]string{
+		"Santa Fe":   "Santa Fe",
+		"Entre Rios": "Parana",
+		"Chaco":      "Resistencia",
+	}
+
+	for pais, capital := range capitales {
+		fmt.Println(pais, capital)
+	}
 }
